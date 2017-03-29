8bfd57e513a66bdb5aef5416da024ce441964562
Michal Srb <michal@redhat.com>
2017-03-29 12:36:56 +0200

    Try to fix rerunOpenShiftJob

--- vars/rerunOpenShiftJob.groovy
+++ vars/rerunOpenShiftJob.groovy
@@ -7,8 +7,8 @@ def call() {
     body.delegate = config
     body()
 
-    sh "oc --context=${cluster} get job ${jobName} --export=true --output=yaml > ${jobName}.yaml"
-    sh "oc --context=${cluster} delete job ${jobName}"
-    sh "oc --context=${cluster} create -f ${jobName}.yaml"
+    sh "oc --context=${config.cluster} get job ${config.jobName} --export=true --output=yaml > ${config.jobName}.yaml"
+    sh "oc --context=${config.cluster} delete job ${config.jobName}"
+    sh "oc --context=${config.cluster} create -f ${config.jobName}.yaml"
 }
 
