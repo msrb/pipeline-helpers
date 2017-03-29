6408ee6f00737e8b54cb35b628792c8bc59eb720
Michal Srb <michal@redhat.com>
2017-03-29 13:06:25 +0200

    Fix signature

--- vars/rerunOpenShiftJob.groovy
+++ vars/rerunOpenShiftJob.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/groovy
 
-def call() {
+def call(body) {
     // evaluate the body block, and collect configuration into the object
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
